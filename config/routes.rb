d5e04603ad00cc7da8073fa82704ddfb1e16ec73
Nick Pomroy <npomroy69@hotmail.com>
2016-12-12 05:46:36 +0000

    Added profiles routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   root to: 'pages#home'
+  devise_for :users
+  resources :users do
+    resource :profile
+  end
   get 'about', to: 'pages#about'
 end
