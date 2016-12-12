6d05fee53a66cb0a56b1df0737c8ec0a4061c861
Nick Pomroy <npomroy69@hotmail.com>
2016-12-12 07:52:17 +0000

    Added before filters to check current user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+   before_action :authenticate_user!
+   before_action :only_current_user
+   
    # get /users/:user_id/profile/new
    def new
        @profile = Profile.new
@@ -39,5 +42,10 @@ class ProfilesController < ApplicationController
    private
         def profile_params
            params.require(:profile).permit(:first_name, :last_name, :contact_email, :avatar, :description) 
+        end
+        
+        def only_current_user
+           @user = User.find(params[:user_id])
+           redirect_to root_path unless @user == current_user
         end
 end
