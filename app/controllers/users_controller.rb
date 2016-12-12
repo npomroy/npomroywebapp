ec022b652dff1aad0ad5ee48591ab567213ccecb
Nick Pomroy <npomroy69@hotmail.com>
2016-12-12 08:01:23 +0000

    Added users index action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,9 @@ class UsersController < ApplicationController
     def show
        @user = User.find(params[:id]) 
     end
+    
+    # GET /users
+    def index
+       @users = User.includes(:profile) 
+    end
 end
