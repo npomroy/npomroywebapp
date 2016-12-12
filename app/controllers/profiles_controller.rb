a38dc4844c3649e48cbd8240a82e5a41825c1ad1
Nick Pomroy <npomroy69@hotmail.com>
2016-12-12 06:23:14 +0000

    Added profile create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,22 @@ class ProfilesController < ApplicationController
    def new
        @profile = Profile.new
    end
+   
+   # POST to /users/:user_id/profile
+   def create
+      @user = User.find(params[:user_id]) 
+      @profile = @user.build_profile(profile_params)
+      if @profile.save
+          flash[:success] = "Profile Created"
+          redirect_to user_path(id: params[:user_id])
+      else
+          flash[:error] = "Creation Failed"
+          render action: :new
+      end
+   end
+   
+   private
+        def profile_params
+           params.require(:profile).permit(:first_name, :last_name, :contact_email, :description) 
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212042242) do
+ActiveRecord::Schema.define(version: 20161212061744) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161212042242) do
     t.text     "description"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
