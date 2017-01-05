d6585c92f85af29c08e5d454d6760988f39233b5
Nick Pomroy <npomroy69@hotmail.com>
2017-01-05 05:29:36 +0000

    Added blogpost table

--- db/migrate/20170105052040_create_blogposts.rb
+++ db/migrate/20170105052040_create_blogposts.rb
@@ -1,7 +1,9 @@
 class CreateBlogposts < ActiveRecord::Migration[5.0]
   def change
     create_table :blogposts do |t|
-
+      t.string :title
+      t.integer :user_id
+      t.text :body
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212064701) do
+ActiveRecord::Schema.define(version: 20170105052040) do
+
+  create_table "blogposts", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
